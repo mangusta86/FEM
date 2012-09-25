9fae9297b45ddd40c974eafd4e983772154901de
Marco Fassi <fassimarco86@gmail.com>
2012-09-25 19:47:34 +0100

    little updates...

--- FEM/gen/com/example/fem/R.java
+++ FEM/gen/com/example/fem/R.java
@@ -18,17 +18,18 @@ public final class R {
         public static final int rettangolo=0x7f020004;
     }
     public static final class id {
-        public static final int button1=0x7f070009;
-        public static final int imageView1=0x7f070005;
-        public static final int matName=0x7f070002;
-        public static final int matPoisson=0x7f070004;
-        public static final int matYoung=0x7f070003;
-        public static final int menu_settings=0x7f07000a;
-        public static final int pager=0x7f070001;
-        public static final int proBase=0x7f070007;
-        public static final int proHeight=0x7f070008;
-        public static final int proName=0x7f070006;
-        public static final int prova=0x7f070000;
+        public static final int button0000=0x7f070000;
+        public static final int button1=0x7f07000a;
+        public static final int imageView1=0x7f070006;
+        public static final int matName=0x7f070003;
+        public static final int matPoisson=0x7f070005;
+        public static final int matYoung=0x7f070004;
+        public static final int menu_settings=0x7f07000b;
+        public static final int pager=0x7f070002;
+        public static final int proBase=0x7f070008;
+        public static final int proHeight=0x7f070009;
+        public static final int proName=0x7f070007;
+        public static final int prova=0x7f070001;
     }
     public static final class layout {
         public static final int activity_element=0x7f030000;
--- FEM/src/com/example/fem/Element.java
+++ FEM/src/com/example/fem/Element.java
@@ -4,102 +4,83 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.OptionalDataException;
 import java.io.StreamCorruptedException;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.fem.Profile.Rectangle;
 
 public class Element extends Activity {
+int a=0;
+public Profile rec=new Profile();
+public Profile.Rectangle rectangle = rec.new Rectangle();
+public Material mat = new Material();
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_element);
-        
-        String FILENAME = "hello_file";
-    	FileInputStream fis = null;
-    	try {
-    		fis = openFileInput(FILENAME);
-    		} catch (FileNotFoundException e) {
-    		// TODO Auto-generated catch block
-    		e.printStackTrace();
-    		}
-    	ObjectInputStream iis = null;
-    	try {
-    	iis = new ObjectInputStream(fis);
-    	} catch (StreamCorruptedException e) {
-    	// TODO Auto-generated catch block
-    	e.printStackTrace();
-    	} catch (IOException e) {
-    	// TODO Auto-generated catch block
-    	e.printStackTrace();
-    	}
-    	Profile rec=new Profile();
-    	try {
-			rec=(Profile) iis.readObject();
-		} catch (OptionalDataException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	Profile.Rectangle rectangle=rec.new Rectangle();
-    	try {
-			rectangle=(Rectangle) iis.readObject();
-		} catch (OptionalDataException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	Material mat=new Material();
-    	try {
-			mat=(Material) iis.readObject();
-		} catch (OptionalDataException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_element);
+	}
 
-    	try {
-			iis.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	try {
-			fis.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	 TextView t = new TextView(this); 
+	@Override
+	public void onResume() {
+		super.onResume();
+		
+		if (a==1) {
+			try {
+				openFile();
+			} catch (StreamCorruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+		
+		TextView t = new TextView(this);
 
-    	    t=(TextView)findViewById(R.id.prova); 
-    	    t.setText(mat.getName()+rectangle.base );
-    }
+		t = (TextView) findViewById(R.id.prova);
+		t.setText(mat.getName() + rectangle.base);
+		}			
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_element, menu);
-        return true;
-    }
+	public void openFile() throws StreamCorruptedException, IOException, ClassNotFoundException{
+
+		
+	String FILENAME1 = "hello_file1";
+	String FILENAME2 = "hello_file2";
+	String FILENAME3 = "hello_file3";
+
+
+	FileInputStream fis1 = openFileInput(FILENAME1);
+	ObjectInputStream iis1 = new ObjectInputStream(fis1);
+	rec=(Profile) iis1.readObject();
+	iis1.close();
+	fis1.close();
+
+	FileInputStream fis2 = openFileInput(FILENAME2);
+	ObjectInputStream iis2 = new ObjectInputStream(fis2);
+	rectangle=(Rectangle) iis2.readObject();
+	iis2.close();
+	fis2.close();
+	
+	FileInputStream fis3 = openFileInput(FILENAME3);
+	ObjectInputStream iis3 = new ObjectInputStream(fis3);
+	mat= (Material) iis3.readObject();
+	iis3.close();
+	fis3.close();		
+	}
+	
+	public void modella(View view) {
+		a=1;
+		Intent myIntent = new Intent(Element.this, SetProperty.class);
+		Element.this.startActivity(myIntent);
+	}
 }
