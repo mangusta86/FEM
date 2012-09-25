e22c332ff1c0d296a3a2827c98c19202dd4d1ecd
Marco Fassi <fassimarco86@gmail.com>
2012-09-25 13:10:22 +0100

    delete navigation tab...

--- FEM/src/com/example/fem/SetProperty.java
+++ FEM/src/com/example/fem/SetProperty.java
@@ -5,185 +5,74 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
-import android.app.ActionBar;
-import android.app.ActionBar.TabListener;
-import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 
-public class SetProperty extends FragmentActivity  implements ActionBar.TabListener {
-
-    SectionsPagerAdapter mSectionsPagerAdapter;
-
-    ViewPager mViewPager;
-
-	ViewPager mViewPager;
+public class SetProperty extends FragmentActivity  {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_property);
-        // Create the adapter that will return a fragment for each of the three primary sections
-        // of the app.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the action bar.
-        final ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        // When swiping between different tab.
-        // We can also use ActionBar.Tab#select() to sections, select the corresponding tdo this if we have a reference to the
-        // Tab.
-        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                actionBar.setSelectedNavigationItem(position);
-            }
-        });
-
-        // For each of the sections in the app, add a tab to the action bar.
-        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-            // Create a tab with text corresponding to the page title defined by the adapter.
-            // Also specify this Activity object, which implements the TabListener interface, as the
-            // listener for when this tab is selected.
-            actionBar.addTab(
-                    actionBar.newTab()
-                            .setText(mSectionsPagerAdapter.getPageTitle(i))
-                            .setTabListener(this));
-        }
-   
-  	 try {
-		onClickSend();
-	} catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter();
+        ViewPager myPager = (ViewPager) findViewById(R.id.pager);
+        myPager.setAdapter(adapter);
+        myPager.setCurrentItem(0);
+    }
+    @Override
+    public void onPause() {	
     }
     
-    @Override
-    public void onPause(){
-		
-    }
-    
+    public class SectionsPagerAdapter extends PagerAdapter {
 
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
-
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        // When the given tab is selected, switch to the corresponding page in the ViewPager.
-        mViewPager.setCurrentItem(tab.getPosition());
-    }
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to one of the primary
-     * sections of the app.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            
+        
+        public Object instantiateItem(View collection, int position) {
+            LayoutInflater inflater = (LayoutInflater) collection.getContext()
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            int resId = 0;
             switch (position) {
-            case 0: 
-            	Fragment fragment1 = new Fragment1();
-            	return fragment1;
-            case 1: 
-            	Fragment fragment2 = new Fragment2();
-            	return fragment2;
-            case 2: 
-            	Fragment fragment3 = new Fragment3();
-            	return fragment3;
+            case 0:
+                resId = R.layout.profile;
+                break;
+            case 1:
+                resId = R.layout.material;
+                break;
+            case 2:
+                resId = R.layout.section;
+                break;
+            }
+            View view = inflater.inflate(resId, null);
+            ((ViewPager) collection).addView(view, 0);
+            return view;
         }
-            return null;
-        }
+
 
         @Override
         public int getCount() {
             return 3;
         }
 
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0: return getString(R.string.title_section1).toUpperCase();
-                case 1: return getString(R.string.title_section2).toUpperCase();
-                case 2: return getString(R.string.title_section3).toUpperCase();
-            }
-            return null;
-        }
+
+		@Override
+		public boolean isViewFromObject(View arg0, Object arg1) {
+			// TODO Auto-generated method stub
+			return arg0 == ((View) arg1);
+		}
+		
+	    @Override
+	    public void destroyItem(View arg0, int arg1, Object arg2) {
+	            ((ViewPager) arg0).removeView((View) arg2);
+	    }      
     }
 
-    /**
-     * A dummy fragment representing a section of the app, but that simply displays dummy text.
-     */
-    public static class Fragment1 extends Fragment {
-        public Fragment1() {
-        }
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.profile, container, false);
-        }
-        @Override
-        public void onPause(){
-        	
-
-        }
-        
-        		
-    }
-    public static class Fragment2 extends Fragment {
-        public Fragment2() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.material, container, false);
-        }
-    }
-    public static class Fragment3 extends Fragment {
-        public Fragment3() {
-        	}
-        
-
-        
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.section, container, false);
-        }
-    }
     
-    public void onClickSend() throws FileNotFoundException{
-    	Button button = (Button) findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+    public void saveData(View view) {
             	
             	Profile rec=new Profile();
             	rec.setName(getText(R.id.proName).toString());
@@ -249,8 +138,7 @@ public class SetProperty extends FragmentActivity  implements ActionBar.TabListe
             	Intent Play = new Intent(SetProperty.this, Element.class);
             	SetProperty.this.startActivity(Play);
             	finish();
-            };
-        });
+
 
     }
 }
